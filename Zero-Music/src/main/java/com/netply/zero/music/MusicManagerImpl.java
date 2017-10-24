f206fc52ff3116f11d7fc7dcb615d2b30ed8d00b
Pawel Maskowicz <gamer.4ply@gmail.com>
2017-10-24 20:23:16 +0200

    Remove debug output

--- Zero-Music/src/main/java/com/netply/zero/music/MusicManagerImpl.java
+++ Zero-Music/src/main/java/com/netply/zero/music/MusicManagerImpl.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,9 +15,7 @@ public class MusicManagerImpl implements MusicManager {
     @Override
     public List<String> musicList() {
         File directory = new File(MUSIC_DIR);
-        System.out.println(Arrays.toString(directory.listFiles()));
         Collection<File> files = FileUtils.listFiles(directory, new String[]{"mp3"}, true);
-        System.out.println(files);
 
         return files.stream().map(File::getName).collect(Collectors.toList());
     }
